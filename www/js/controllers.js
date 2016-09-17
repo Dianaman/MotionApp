d5531c2f81033fd78bbea2a68c112e14ba9f64a7
dianaman <diana.man.91@gmail.com>
2016-09-17 11:52:20 -0300

    muestreo de coordenadas

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,49 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope, $cordovaDeviceMotion, $ionicPlatform, $window) {
+  try{
+    $ionicPlatform.ready(function() {
+      /*$cordovaDeviceMotion.getCurrentAcceleration().then(function(result) {
+          $scope.x = result.x;
+          $scope.y = result.y;
+          $scope.z = result.z;
+          $scope.timeStamp = result.timestamp;
+        }, function(err) {
+          // An error occurred. Show a message to the user
+      });*/
+
+      // watch Acceleration
+      var options = { frequency: 20000 };
+
+      var watch = $cordovaDeviceMotion.watchAcceleration(options);
+      watch.then(
+        null,
+        function(error) {
+        // An error occurred
+        },
+        function(result) {
+          $scope.x = result.x;
+          $scope.y = result.y;
+          $scope.z = result.z;
+          $scope.timeStamp = result.timestamp;
+      });
+
+/*
+      watch.clearWatch();
+      // OR
+      $cordovaDeviceMotion.clearWatch(watch)
+        .then(function(result) {
+          // success
+          }, function (error) {
+          // error
+      });*/
+
+    })
+  }
+  catch(e){
+    alert(e.getMessage());
+  }
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
